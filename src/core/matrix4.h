de754e8ad97c33ed1840546a6d46385f7a892955
Florian Ziesche <florian.ziesche@gmail.com>
2012-07-07 18:41:32 +0200

    * when constructing a matrix4 from another matrix4 with a different type, cast the data values

--- src/core/matrix4.h
+++ src/core/matrix4.h
@@ -57,10 +57,10 @@ public:
 		((T*)&col_3)[0], ((T*)&col_3)[1], ((T*)&col_3)[2], ((T*)&col_3)[3] }}) {}
 	template <typename U> constexpr matrix4(const matrix4<U>& mat4) noexcept :
 	data({{
-		mat4.data[0], mat4.data[1], mat4.data[2], mat4.data[3],
-		mat4.data[4], mat4.data[5], mat4.data[6], mat4.data[7],
-		mat4.data[8], mat4.data[9], mat4.data[10], mat4.data[11],
-		mat4.data[12], mat4.data[13], mat4.data[14], mat4.data[15] }}) {}
+		(T)mat4.data[0], (T)mat4.data[1], (T)mat4.data[2], (T)mat4.data[3],
+		(T)mat4.data[4], (T)mat4.data[5], (T)mat4.data[6], (T)mat4.data[7],
+		(T)mat4.data[8], (T)mat4.data[9], (T)mat4.data[10], (T)mat4.data[11],
+		(T)mat4.data[12], (T)mat4.data[13], (T)mat4.data[14], (T)mat4.data[15] }}) {}
 	
 	matrix4& operator=(const matrix4& mat) {
 		memcpy(&data[0], &mat.data[0], sizeof(T)*16);
