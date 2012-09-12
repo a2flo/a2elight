220d3e2c80bd43dd9144b973e185271c10892ff9
Florian Ziesche <florian.ziesche@gmail.com>
2012-09-12 13:24:42 +0200

    * added cuda lib paths and link against cudart to premake file

--- premake4.lua
+++ premake4.lua
@@ -137,6 +137,8 @@ project "a2elight"
 			add_include("/usr/local/cuda/include")
 			add_include("/usr/local/cuda-5.0/include")
 			defines { "A2E_CUDA_CL=1" }
+			libdirs { "/opt/cuda-toolkit/lib", "/usr/local/cuda/lib", "/usr/local/cuda-5.0/lib" }
+			links { "cudart" }
 		end
 	end
 	
